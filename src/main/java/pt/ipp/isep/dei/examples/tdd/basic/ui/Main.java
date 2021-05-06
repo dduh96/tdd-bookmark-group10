7e4dcc85decb80a1b5cc2997a5f0671db6f1b002
Katharina <katharina.buehn@hotmail.com>
2021-05-06 12:59:38 +0200

    ********** Setup Bookmark Project ********** new

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -1,25 +1,9 @@
 package pt.ipp.isep.dei.examples.tdd.basic.ui;
 
-import pt.ipp.isep.dei.examples.tdd.basic.domain.Calculator;
-import pt.ipp.isep.dei.examples.tdd.basic.domain.Puzzle;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(3, 4));
-
-        Puzzle puzzle = new Puzzle();
-        Scanner scan = new Scanner(System.in);
-        while (true){
-            System.out.println("Enter your puzzle instructions without delimiters (r,f,p): \n(q) for quit");
-            String input = scan.next();
-            if(input.toLowerCase().equals("q")) break;
-
-            System.out.println(puzzle.printBoard(input));
-
-        }
+        
     }
 }
